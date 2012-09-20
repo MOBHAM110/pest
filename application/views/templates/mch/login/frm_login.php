159bb8405b6bb95375a24b19352167099827b63f
Leo Nguyen <nam85it@gmail.com>
2012-09-20 15:46:47 +0700

    Fix

--- application/views/templates/mch/login/frm_login.php
+++ application/views/templates/mch/login/frm_login.php
@@ -9,7 +9,7 @@
     <?php } else { ?> 
     <table  align="center" cellpadding="5" cellspacing="0">        <tr><td align="center"><input type="text" autocomplete="off" class="input_login"  name="txt_username" id="txt_username" onfocus="if(this.value=='<?php echo Kohana::lang('account_lang.lbl_username')?>') { this.value = ''; this.style.color='#666';}" onblur="if(this.value=='') { this.value = '<?php echo Kohana::lang('account_lang.lbl_username')?>'; this.style.color='#CCC';}" value="<?php echo Kohana::lang('account_lang.lbl_username')?>" style="color:#CCC"></td></tr>
     <tr><td align="center"><input type="password" autocomplete="off" class="input_login" name="txt_pass" id="txt_pass" onfocus="if(this.value=='<?php echo Kohana::lang('account_lang.lbl_pass')?>') { this.value = ''; this.style.color='#666';}" onblur="if(this.value=='') { this.value = '<?php echo Kohana::lang('account_lang.lbl_pass')?>'; this.style.color='#CCC';}" value="<?php echo Kohana::lang('account_lang.lbl_pass')?>"></td></tr> 
-    <tr><td class="login_content_btn" align="center"><button type="submit" class="button_login" style="height:21px; width:80px; border:none;"><?php echo Kohana::lang('login_lang.btn_login')?></button></td>
+    <tr><td class="login_content_btn" align="center"><button type="submit" class="button"><?php echo Kohana::lang('login_lang.btn_login')?></button></td>
     </tr>
     <tr>
     <td class="login_content_a" align="center"><a href="<?php echo url::base()?>forgotpass"><?php echo Kohana::lang('account_lang.lbl_forgot_pass')?>?</a><span style="color:#999; padding:0px 5px;">|</span><a href="<?php echo url::base()?>register"><?php echo Kohana::lang('account_lang.lbl_register')?></a></td>
@@ -18,5 +18,5 @@
     <?php } // end if check login ?>      
     </form>
 </td></tr>
-<tr><td class="login_block_bottom">&nbsp;</td></tr>  
+<tr><td class="login_block_bottom"></td></tr>  
 </table>
