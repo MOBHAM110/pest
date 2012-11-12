3f677c591acb90a6acb291ab4769ed6fe6158d1d
Leo Nguyen <nam85it@gmail.com>
2012-11-12 16:38:51 +0700

    fix db config

--- application/config/database.php
+++ application/config/database.php
@@ -40,16 +40,7 @@ $config['default'] = array
 (
 	'benchmark'     => TRUE,
 	'persistent'    => FALSE,
-	'connection'    => array
-	(
-		'type' => 'mysql',
-		'user' => 'root',
-		'pass' => '',
-		'host' => '',
-		'port'     => FALSE,
-		'socket'   => FALSE,
-		'database' => 'mch_pest'
-	),
+	'connection'    => 'mysql://root:@localhost/mch_pest',
 	'character_set' => 'utf8',
 	'table_prefix'  => '',
 	'object'        => TRUE,
